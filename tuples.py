032c5a4eebe68dff64e0fb51c2df940c4e790825
avnighai2 <avnighai2@gmail.com>
2024-12-10 18:35:54 -0500

    module 4 lesson 2

---
+++ tuples.py
@@ -0,0 +1,19 @@
+
+
+
+tuplex = ("tuple", False, 3,2,1)
+print(tuplex)
+
+tuplex = (4, 6 ,2 ,8, 3, 1)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (30,40,50,10,50)
+print(tuple1.count(50))
+
+tuplex = (2, 3, 4, 5 ,6, 7, 8, 9, 10, 11)
+Slice = tuplex[3:5]
+print(Slice)
+Slice = tuplex[:7]
+print(Slice)
